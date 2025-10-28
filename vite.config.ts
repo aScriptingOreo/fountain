c7a1c7f978ee6c98796b9b8ba62fdf5861dc33f2
aScriptingOreo <ksls56@hotmail.com>
2025-10-28 12:33:49 +0000

    fixed docker-compose.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), UnoCSS()]
+	plugins: [sveltekit(), UnoCSS()],
+	preview: {
+		host: '0.0.0.0',
+		port: 3000
+	}
 });
